9dfb022deabc57cb11ece0e676301b5b103dde33
daniel03f <daniel.fehrenbacher@alumni.fh-aachen.de>
2024-04-15 11:59:11 +0200

    In query console von abalo@localhost: CREATE USER dev WITH PASSWORD 'dbwt2' .env: DB_USERNAME=dev DB_PASSWORD=dbwt2 AuthController.php unter app/Http/Controllers kopiert Web.php C&P in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get('/testdata',[\App\Http\Controllers\AbTestDataController::class,'index']);
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::get('/isloggedin', [App\Http\Controllers\AuthController::class, 'isloggedin'])->name('haslogin');
