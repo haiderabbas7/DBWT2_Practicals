193b909e55b73ef966ef13c48955a3a74f562a0d
daniel03f <daniel.fehrenbacher@alumni.fh-aachen.de>
2024-04-16 12:16:20 +0200

    A9) Seeding DevelopmentData.php mit Null Fehler in seedTable

--- database/seeders/DevelopmentData.php
+++ database/seeders/DevelopmentData.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DevelopmentData extends Seeder
 {
@@ -12,6 +15,29 @@ class DevelopmentData extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->seedTable('ab_articlecategory', '../abalo/resources/misc/articlecategory.csv');
+        $this->seedTable('ab_article', '../abalo/resources/misc/articles.csv');
+        $this->seedTable('ab_user', '../abalo/resources/misc/user.csv');
+    }
+
+    /**
+     * Opens the csv file and extracts the data, then puts them into the corresponding table
+     *
+     * @param string $tableName The table to be inserted into
+     * @param string $fileName The file to open
+     * @return void
+     */
+    private function seedTable(string $tableName, string $fileName) : void
+    {
+        if(($handle = fopen($fileName, 'r')) !== false) {
+            $header = fgetcsv($handle, 1000, ';'); // column names
+            while($data = fgetcsv($handle, 1000 , ';')) {
+                $data = array_map(function($value) { return $value === 'NULL' ? null : $value; }, $data); // Replace 'NULL' with null
+                $data = array_map('utf8_encode', $data); // Encode data to UTF-8
+                $row = array_combine($header, $data);
+                DB::table($tableName)->insert($row);
+            }
+        }
+        fclose($handle);
     }
 }
