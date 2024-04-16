e1716bba135a1bb383a8f62b738d19963f48c421
daniel03f <daniel.fehrenbacher@alumni.fh-aachen.de>
2024-04-16 10:53:52 +0200

    Seeding Datei

---
+++ database/seeders/DevelopmentData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevelopmentData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
