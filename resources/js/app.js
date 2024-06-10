0de8510d0a90686716506d911edde28958252012
HaiderAbbasFH <haider.abbas@alumni.fh-aachen.de>
2024-06-10 14:21:02 +0200

    M4 aufg.7 - App für Vue eingestellt

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,10 @@ import './bootstrap';
 import './data.js'
 import './cookiecheck.js'
 import './shoppingcart.js'
+
+import { createApp } from 'vue';
+const vm = createApp({
+    data() {
+        return { msg: 'a!' }
+    }
+}).mount('#app');
