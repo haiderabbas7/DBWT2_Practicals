65ed86e8c2494c657f6856d35c1e6378b3480bf3
HaiderAbbasFH <haider.abbas@alumni.fh-aachen.de>
2024-06-10 15:47:36 +0200

    Aufgabe 5 + kleiner hotfix an den Migrations weil ich aus versehen die anderen tabellen gelöscht habe

--- database/seeders/DevelopmentData.php
+++ database/seeders/DevelopmentData.php
@@ -4,13 +4,9 @@ namespace Database\Seeders;
 
 
 use App\Helpers\UserHelper;
-use App\Models\Article;
-use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class DevelopmentData extends Seeder
 {
@@ -19,11 +15,10 @@ class DevelopmentData extends Seeder
      */
     public function run(): void
     {
-        //$this->seed_benutzer();
+        $this->seed_benutzer();
         $this->seed_articles();
         $this->seed_articlecategories();
-        //WENN ES PROBLEME MIT DEN SEQUENZEN GIBT, SCHREIB MIR EINE METHODE ZUM MANUELLEN UPDATEN DER SEQUENZEN
-        //$this->updateAllIDSequences();
+        $this->seed_article_has_articlecategories();
     }
 
     private function read_csv_into_array(string $fileName) : array
@@ -48,8 +43,6 @@ class DevelopmentData extends Seeder
         $arr = $this->read_csv_into_array('user.csv');
         for($i = 1; $i < sizeof($arr); $i++){
             DB::table('benutzer')->insert([
-                //MUSS ENTFERNT WERDEN, DAMIT DIE SEQUENZEN BENUTZT WERDEN
-                //'id' => $arr[$i][0],
                 'name' => $arr[$i][1],
                 'password' => sha1(UserHelper::get_salt() . $arr[$i][2]),
                 'mail' => $arr[$i][3]
@@ -66,8 +59,6 @@ class DevelopmentData extends Seeder
             $formattedTimestamp = $unformattedTimestamp->format('Y-m-d H:i:s');
 
             DB::table('article')->insert([
-                //MUSS ENTFERNT WERDEN, DAMIT DIE SEQUENZEN BENUTZT WERDEN
-                //'id' => $arr[$i][0],
                 'name' => $arr[$i][1],
                 'price' => intval($arr[$i][2]),
                 'description' => $arr[$i][3],
@@ -82,12 +73,19 @@ class DevelopmentData extends Seeder
         $arr = $this->read_csv_into_array('articlecategory.csv');
         for($i = 1; $i < sizeof($arr); $i++){
             DB::table('articlecategory')->insert([
-                //MUSS ENTFERNT WERDEN, DAMIT DIE SEQUENZEN BENUTZT WERDEN
-                //'id' => $arr[$i][0],
                 'name' => $arr[$i][1],
                 'parent' => ($arr[$i][2] === "NULL") ? null : $arr[$i][2]
             ]);
         }
     }
-
+    private function seed_article_has_articlecategories()
+    {
+        $arr = $this->read_csv_into_array('article_has_articlecategory.csv');
+        for($i = 1; $i < sizeof($arr); $i++){
+            DB::table('article_has_category')->insert([
+                'articlecategory_id' => $arr[$i][0],
+                'article_id' => $arr[$i][1]
+            ]);
+        }
+    }
 }
