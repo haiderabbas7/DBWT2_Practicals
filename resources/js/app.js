ed808ed92531f18f4748c12dfbfd22131c4e5e0f
HaiderAbbasFH <haider.abbas@alumni.fh-aachen.de>
2024-06-12 20:38:27 +0200

    aufg 12 done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import './bootstrap';
 import './data.js'
 import './cookiecheck.js'
@@ -7,17 +9,43 @@ import { createApp } from 'vue';
 const vm = createApp({
     data() {
         return {
-            msg: 'aaa!',
-            newArticle_csrfToken: "",
-            newArticle_name: "",
-            newArticle_price: "",
-            newArticle_description: "",
-            newArticle_status: ""
+            newArticle_csrfToken: '',
+            newArticle_name: '',
+            newArticle_price: '',
+            newArticle_description: '',
+            newArticle_status_color: 'green',
+            newArticle_status_text: ''
         }
     },
     methods: {
+        //der gleiche code wie im Button EventListener, nur natürlich etwas angepasst
         sendNewArticleInfo: function (){
-
+            let name = this.$data.newArticle_name;
+            let price = this.$data.newArticle_price;
+            let description = this.$data.newArticle_description;
+            //Clientseitige Checks
+            if(name === '' || isNaN(price) || price <= 0){
+                this.newArticle_status_color = 'red';
+                this.newArticle_status_text = '<b>FEHLER</b>: Bitte geben Sie gültige Werte ein: Kein leerer Name und nur positive Werte für Preis';            }
+            else{
+                let xhr = new XMLHttpRequest();
+                xhr.open('POST', '/api/articles');
+                //hier per Arrow funktion wird this auf das vue objekt gebunden
+                xhr.onload = () => {
+                    let response = JSON.parse(xhr.responseText);
+                    this.newArticle_status_text = response.message;
+                    this.newArticle_status_color = 'green';
+                    if(response.status === 'Fehler'){
+                        this.newArticle_status_color = 'red';
+                    }
+                };
+                let formData = new FormData();
+                formData.append("name", name);
+                formData.append("price", price);
+                formData.append("description", description);
+                xhr.send(formData);
+            }
+            return false;
         }
     }
 }).mount('#app');
