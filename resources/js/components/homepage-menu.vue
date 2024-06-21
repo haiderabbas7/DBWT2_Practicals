2d8ea56daee17a801edb463bbc0357ca0f16c4fa
HaiderAbbasFH <haider.abbas@alumni.fh-aachen.de>
2024-06-21 19:30:07 +0200

    aufg 4 done

--- resources/js/components/homepage-menu.vue
+++ resources/js/components/homepage-menu.vue
@@ -3,7 +3,7 @@ export default {
     name: "homepage-menu",
     data: function () {
         return {
-            index_menu: {
+            index_SPA_menu: {
                 "Home": [],
                 "Kategorien": [],
                 "Verkaufen": [],
@@ -11,14 +11,34 @@ export default {
             }
         }
     },
-    methods: {
-
+    mounted() {
+        //Kategorien werden per API Call geladen, nicht mehr über Controller umweg
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/kategorien');
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                this.index_SPA_menu.Kategorien = data.map(kategorie => kategorie.name);
+            }
+        };
+        xhr.send();
     }
 }
 </script>
 
 <template>
-
+    <nav>
+        <ul>
+            <li v-for="(values, key) in index_SPA_menu" :key="key">
+                {{ key }}
+                <ul v-if="values.length > 0">
+                    <li v-for="(value, index) in values" :key="index">
+                        {{ value }}
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <style scoped>
